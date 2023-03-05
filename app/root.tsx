75e8b6e247626b24a11894357919de1715a13a3c
cpro95 <cpro95@gmail.com>
2023-03-05 18:26:42 +0900

    google analytics, naver search advisor, daum web master

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import {
   ScrollRestoration,
   useCatch,
   useLoaderData,
+  useLocation,
 } from "@remix-run/react";
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
@@ -28,6 +29,7 @@ import { RecoilRoot } from "recoil";
 import { useTranslation } from "react-i18next";
 import i18next from "~/i18next.server";
 import { useEffect } from "react";
+import * as gtag from "~/utils/gtags.client";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
@@ -76,6 +78,12 @@ export let handle = {
 function App({ locale }: { locale: string }) {
   const [theme] = useTheme();
   invariant(theme, "theme must be set");
+
+  const location = useLocation();
+  useEffect(() => {
+    gtag.pageview(location.pathname, "G-KZHJ0LLR0H");
+  }, [location]);
+
   let { i18n } = useTranslation();
 
   // This hook will change the i18n instance language to the current locale
@@ -89,6 +97,52 @@ function App({ locale }: { locale: string }) {
       <head>
         <Meta />
         <Links />
+
+        <meta
+          name="naver-site-verification"
+          content="14f64250bf24454949ee28c13454547324c8f93c"
+        />
+        <meta name="NaverBot" content="All" />
+        <meta name="NaverBot" content="index,follow" />
+        <meta name="Yeti" content="All" />
+        <meta name="Yeti" content="index,follow" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KZHJ0LLR0H"
+        ></script>
+        <script
+          async
+          id="gtag-init"
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-KZHJ0LLR0H', {
+                  page_path: window.location.pathname,
+                });
+              `,
+          }}
+        />
       </head>
       <body>
         <Outlet />
@@ -127,29 +181,6 @@ export function ErrorBoundary(props: any) {
         <title>myKpop</title>
         <Meta />
         <Links />
-        <meta charSet="utf-8" />
-        <meta name="NaverBot" content="All" />
-        <meta name="NaverBot" content="index,follow" />
-        <meta name="Yeti" content="All" />
-        <meta name="Yeti" content="index,follow" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body>
         <section className="flex h-full items-center bg-gray-50 text-gray-800 sm:p-16">
